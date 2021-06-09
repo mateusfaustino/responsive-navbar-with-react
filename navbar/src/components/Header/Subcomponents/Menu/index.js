7b12f059a91082bf8dd65298787422a75555d2e4
Mateus Faustino <mateusfaustino2017@gmail.com>
2021-06-08 21:49:31 -0300

    the project is oganized now

--- navbar/src/components/Header/Subcomponents/Menu/index.js
+++ navbar/src/components/Header/Subcomponents/Menu/index.js
@@ -1,13 +1,13 @@
 import './style.css';
 import {Link} from 'react-router-dom';
 
-const Menu = () => {
+const Menu = (props) => {
     return (
         <ul className='nav__menu'>
-            <li><Link  to='/' className='nav__menu__item'>home</Link></li>
-            <li><Link  to='/about' className='nav__menu__item'>about</Link></li>
-            <li><Link  to='/contact' className='nav__menu__item'>contact</Link></li>
-            <li><Link  to='/buy' className='nav__menu__item'>buy</Link></li>     
+            <li onClick={props.setIsActive}><Link  to='/' className='nav__menu__item'>home</Link></li>
+            <li onClick={props.setIsActive}><Link  to='/about' className='nav__menu__item'>about</Link></li>
+            <li onClick={props.setIsActive}><Link  to='/contact' className='nav__menu__item'>contact</Link></li>
+            <li onClick={props.setIsActive}><Link  to='/buy' className='nav__menu__item'>buy</Link></li>     
         </ul>
     )
 }
--- navbar/src/components/Header/index.js
+++ navbar/src/components/Header/index.js
@@ -1,6 +1,5 @@
 import React,{useState} from 'react';
 import './style.css';
-import {Link} from 'react-router-dom';
 import { FaBars} from 'react-icons/fa';
 import { MdClose } from 'react-icons/md';
 import Logo from "./Subcomponents/Logo/index"
@@ -12,7 +11,7 @@ const Navbar = () => {
         <>
             <div className={isActive? "nav active": "nav"}>
                 <Logo/>
-                <Menu onClick={()=>setIsActive(false)}/>
+                <Menu setIsActive={()=>setIsActive(false)}/>
                 <MdClose onClick={()=>setIsActive(!isActive)} className='nav__icon nav__close' color=''/>
                 <FaBars onClick={()=>setIsActive(!isActive)} className='nav__icon nav__bars' color=''/>
             </div>
